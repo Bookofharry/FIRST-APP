b8385cd2a2215b1a1a6f89c4e99389e2644eb00e
DEV HARRY👌 <Bookofharry@hotmail.com>
2025-07-23 05:31:03 +0100

    First commit

---
+++ app/ui/dist/fonts.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.inter = exports.lusitana = void 0;
+var google_1 = require("next/font/google");
+var google_2 = require("next/font/google");
+exports.lusitana = google_2.Lusitana({
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-lusitana'
+});
+exports.inter = google_1.Inter({ subsets: ['latin'] });
